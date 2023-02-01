f487a4ccd71a3f520d1b3055a907406291bdd9a8
Pedro1Developer <sky876613@gmail.com>
2023-02-01 10:01:52 -0300

    creat class Aluno

---
+++ Curso-Java/src/com/pedro/cursojava/aula33/labs/Aluno.java
@@ -0,0 +1,11 @@
+package com.pedro.cursojava.aula33.labs;
+
+public class Aluno {
+	
+	private String nome;
+	private String matricula;
+	private String curso;
+	private String[] diciplinas;
+	private double[][] notas;
+
+}
