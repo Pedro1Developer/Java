f657f7eb987af42483737af79d22ab11c1eaa1b5
Pedro <sky876613@gmail.com>
2023-05-07 20:34:20 -0300

    aula51-teórica

---
+++ src/com/pedro/cursojava/aula51/AaNullPointException.java
@@ -0,0 +1,17 @@
+package com.pedro.cursojava.aula51;
+
+public class AaNullPointException {
+	
+	private String i;
+
+	public String getI() {
+		return i;
+	}
+
+	public void setI(String i) {
+		this.i = i;
+	}
+
+	
+
+}
